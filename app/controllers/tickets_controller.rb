c75d05a18f31ab61c13bcc16f9fc36600516888e
vjdavid <vjdavid621@gmail.com>
2015-04-06 22:25:02 -0700

    json api added to controller tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,39 +1,27 @@
 class TicketsController < ApplicationController
+  before_action :find_proyect
+	before_action :find_ticket, except: [:index, :create]
 
-	before_action :find_proyect
-	before_action :find_ticket, except: [:index, :new, :create]
-
-	def index
-		@tickets = @proyect.tickets
-	end
-
-	def new
-		@ticket = @proyect.tickets.new
+  def index
+    @tickets = @proyect.tickets
+		respond_with @tickets
 	end
 
 	def create
 		@ticket = @proyect.tickets.create(params_ticket)
 		@ticket.save
-
-		redirect_to proyect_tickets_path
-	end
-
-	def edit
 	end
 
 	def show
+		respond_with @ticket  
 	end
 
 	def update					
 		@ticket.update(params_ticket)
-
-		redirect_to proyect_tickets_path(@ticket)
 	end
 
 	def destroy
 		@ticket.destroy
-
-		redirect_to proyect_tickets_path
 	end
 
 	private
