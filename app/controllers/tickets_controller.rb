a0e89577a76f8a29d5c04eea94fcc7a35123f773
vjdavid <vjdavid@gmail.com>
2015-04-17 14:53:18 -0700

    eliminated blank spaces

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,41 +1,40 @@
 class TicketsController < ApplicationController
   before_action :find_proyect
-	before_action :find_ticket, except: [:index, :create]
+  before_action :find_ticket, except: [:index, :create]
 
   def index
     @tickets = @proyect.tickets
-		respond_with @tickets
-	end
+    respond_with @tickets
+  end
 
-	def create
-		@ticket = @proyect.tickets.create(params_ticket)
-		@ticket.save
-	end
+  def create
+    @ticket = @proyect.tickets.create(params_ticket)
+    @ticket.save
+  end
 
-	def show
-		respond_with @ticket  
-	end
+  def show
+    respond_with @ticket
+  end
 
-	def update					
-		@ticket.update(params_ticket)
-	end
+  def update
+    @ticket.update(params_ticket)
+  end
 
-	def destroy
-		@ticket.destroy
-	end
+  def destroy
+    @ticket.destroy
+  end
 
-	private
+  private
+  def find_proyect
+    @proyect = Proyect.find(params[:proyect_id])
+  end
 
-	def find_proyect 
-		@proyect = Proyect.find(params[:proyect_id])
-	end
+  def find_ticket
+    @ticket = @proyect.tickets.find(params[:id])
+  end
 
-	def find_ticket
-		@ticket = @proyect.tickets.find(params[:id])
-	end
-
-	def params_ticket
-		params.require(:ticket).permit(:description, :name)
-	end
+  def params_ticket
+    params.require(:ticket).permit(:description, :name)
+  end
 
 end
